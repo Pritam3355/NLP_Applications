f684e154790685b32f72e93e9ac14548a97a1367
Pritam Das <69068731+Pritam3355@users.noreply.github.com>
2024-12-21 22:59:53 +0530

    Add files via upload

--- ChatBot/chatbot-db.sql
+++ ChatBot/chatbot-db.sql
@@ -1,6 +1,31 @@
 -- use ChatDB;
-select * from ChatDB.Chat_data;
-select * from ChatDB.Chat_history order by chat_id desc;
 
--- delete from ChatDB.Chat_data;
--- delete from ChatDB.Chat_history;
+-- create table if not exists ChatDB.Active_Users(
+-- 		u_id varchar(16),
+--         is_active int);
+-- create table if not exists ChatDB.Session_Data(
+-- 		u_id varchar(16),
+--         session_id varchar(64),
+--         time_stamp datetime,
+--         stream_id int);
+-- create table if not exists ChatDB.Chat_Data(
+-- 		u_id varchar(16),
+--         session_id varchar(64),
+--         user_input text,
+--         bot_response text);
+
+-- insert into ChatDB.Active_Users(u_id,is_active) values 
+-- 	('user123', 0),
+--     ('user_A', 0),
+--     ('user456', 0),
+--     ('user_B', 0),
+--     ('user789', 0);
+--     
+--     
+select * from ChatDB.Active_Users;
+select * from ChatDB.Session_Data;
+select * from ChatDB.Chat_Data;
+
+-- delete from ChatDB.Active_Users;
+-- delete from ChatDB.Session_Data;
+-- delete from ChatDB.Chat_Data;
